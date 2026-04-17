40a61c270169ac6c82cf1fd706e1ca734fdecec0
Martin Cutillo-Rigby <70958005+mcrigby@users.noreply.github.com>
2026-04-17 21:56:38 +0100

    InterruptStatusRegister rename.

--- src/Tca8418InterruptStatusRegister.cs
+++ src/Tca8418InterruptStatusRegister.cs
@@ -1,7 +1,7 @@
 namespace CutilloRigby.Devices.TCA8418;
 
 [Flags]
-public enum Tca8418InterrupStatusRegister : byte
+public enum Tca8418InterruptStatusRegister : byte
 {
     CAD_INT = 0x10,      ///< Ctrl-alt-del seq status
     OVR_FLOW_INT = 0x08, ///< Overflow interrupt status
